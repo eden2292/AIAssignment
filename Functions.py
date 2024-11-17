da87a1213006dcea11f50072f5d284b3d3a325ce
eden2292 <kp798@student.aru.ac.uk>
2024-11-17 11:31:40 +0000

    Changed file name to Functions. Added best parameter judge for DecisionTreeClassifier. Started outcome predicion model for user input.

--- Functions.py
+++ Functions.py
@@ -1,4 +1,6 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
 
 def accuracy(y_test, y_pred, value_count):
     accuracy = accuracy_score(y_test, y_pred)
@@ -13,3 +15,9 @@ def accuracy(y_test, y_pred, value_count):
           f"Precision: {precision} \n"
           f"Recall: {recall} \n"
           f"F1 Score: {f1}")
+    
+def predict_outcome(dataset):
+    IVD_outcome = []
+    for i, column in enumerate(dataset.columns[:-1]):
+        value = float(input(f"{column}"))
+        IVD_outcome.append(value)

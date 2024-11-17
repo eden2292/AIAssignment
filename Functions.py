a3b44c6fd0ba0c855518c487156eef781756bd3a
eden2292 <kp798@student.aru.ac.uk>
2024-11-17 16:19:23 +0000

    Added predictor

--- Functions.py
+++ Functions.py
@@ -1,6 +1,8 @@
+import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
 
 def accuracy(y_test, y_pred, value_count):
     accuracy = accuracy_score(y_test, y_pred)
@@ -16,8 +18,19 @@ def accuracy(y_test, y_pred, value_count):
           f"Recall: {recall} \n"
           f"F1 Score: {f1}")
     
-def predict_outcome(dataset):
-    IVD_outcome = []
+def predict_outcome(dataset, trained_classifier):
+    ivd_outcome = []
+    print(f'Enter prediction variables:')
     for i, column in enumerate(dataset.columns[:-1]):
         value = float(input(f"{column}"))
-        IVD_outcome.append(value)
+        ivd_outcome.append(value)
+
+    ivd_outcome_df = pd.DataFrame([ivd_outcome], columns=dataset.columns[:-1])
+
+    crash_prediction = trained_classifier.predict(ivd_outcome_df)
+    if crash_prediction == 1:
+        outcome = 'Crash'
+    else:
+        outcome = 'No crash'
+
+    return outcome

7a7f580795a925354b314e958a5f19a8ec059cd3
eden2292 <KP798@student.aru.ac.uk>
2024-11-19 15:12:59 +0000

    Added comments to code.

--- Functions.py
+++ Functions.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
+# function to print accuracy metrics
 def accuracy(y_test, y_pred, value_count):
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
@@ -17,17 +18,22 @@ def accuracy(y_test, y_pred, value_count):
           f"Precision: {precision} \n"
           f"Recall: {recall} \n"
           f"F1 Score: {f1}")
-    
+
+# function to predict outcomes - could be used with any classifier.    
 def predict_outcome(dataset, trained_classifier):
+    # create a list to hold predictions
     ivd_outcome = []
+    # request user input
     print(f'Enter prediction variables:')
+    # require information for each column in the CSV
     for i, column in enumerate(dataset.columns[:-1]):
         value = float(input(f"{column}"))
         ivd_outcome.append(value)
-
+    # make the user input data into a dataframe to use with the trained model
     ivd_outcome_df = pd.DataFrame([ivd_outcome], columns=dataset.columns[:-1])
-
+    # make a prediction with the trained model
     crash_prediction = trained_classifier.predict(ivd_outcome_df)
+    # print the outcome
     if crash_prediction == 1:
         outcome = 'Crash'
     else:
